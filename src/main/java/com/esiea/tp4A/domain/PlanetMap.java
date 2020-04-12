a5ae8ea084ac5ebdb767057f95a54f6e1827e3f4
Charles MAZIARSKI <maziarski@et.esiea.fr>
2020-04-12 21:24:15 +0200

    PlanetMap fixed.

--- src/main/java/com/esiea/tp4A/domain/PlanetMap.java
+++ src/main/java/com/esiea/tp4A/domain/PlanetMap.java
@@ -5,4 +5,26 @@ import java.util.Set;
 public interface PlanetMap {
 
     Set<Position> obstaclePositions();
+
+    void deleteObstacle(Position obstaclePosition);
+
+    final class Map implements PlanetMap {
+
+        private final Set<Position> obstacles;
+
+        public Map(Set<Position> obstacles) {
+            this.obstacles = obstacles;
+        }
+
+        @Override
+        public Set<Position> obstaclePositions() {
+            return this.obstacles;
+        }
+
+        @Override
+        public void deleteObstacle(Position obstaclePosition) {
+            obstacles.removeIf(position -> position.getX() == obstaclePosition.getX() && position.getY() == obstaclePosition.getY());
+        }
+
+    }
 }
