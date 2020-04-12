426e4ef4066d44a00019ad81063d8a0598293cb3
dimitri3A <fotsodimitri96@gmail.com>
2020-04-12 21:23:12 +0200

    Main class MovingRover updated.

--- src/main/java/com/esiea/tp4A/domain/MovingRover.java
+++ src/main/java/com/esiea/tp4A/domain/MovingRover.java
@@ -2,121 +2,68 @@ package com.esiea.tp4A.domain;
 
 public class MovingRover implements MarsRover {
 
+    private final Position position;
+    private final PlanetMap planetMap;
+    private final int laserRange;
 
-    private Position position;
-    private  int laserRange;
-    private PlanetMap planetMap;
-    MovingRover(int x, int y, Direction direction) {
-        position = Position.of(x,y,direction);
+    public MovingRover(Position position, int laserRange, PlanetMap planetMap) {
+        this.position = position;
+        this.laserRange = laserRange;
+        this.planetMap = planetMap;
+    }
+@Override
+    public MarsRover initPos(Position position) {
+        return new MovingRover(position, this.laserRange, planetMap);
+    }
+@Override
+    public MarsRover reviseMap(PlanetMap planetMap) {
+        return new MovingRover(this.position, this.laserRange, planetMap);
+    }
+@Override
+    public MarsRover setLaserConf(int range) {
+        return new MovingRover(this.position, range, planetMap);
     }
 
     @Override
     public Position move(String command) {
-
-        int xPosition = position.getX();
-        int yPosition = position.getY();
-        Direction direction = position.getDirection();
-        //String dPosition = direction;
-        switch(position.getDirection()) {
-
-            case NORTH:
-                switch ((command.toLowerCase())) {
-                    case "f":
-                        yPosition = yPosition + 1;
-
-                        break;
-                    case "b":
-                        yPosition = yPosition - 1;
-
-                        break;
-                    case "l":
-                        direction = Direction.WEST;
-
-                        break;
-                    case "r":
-                        direction = Direction.EAST;
-
-                        break;
-                }
-                break;
-
-            case EAST:
-                switch ((command.toLowerCase())) {
-                    case "f":
-                        xPosition = xPosition + 1;
-
-
-                        break;
-                    case "b":
-                        xPosition = xPosition - 1;
-
-
-                        break;
-                    case "l":
-                        direction = Direction.NORTH;
-
-
-                        break;
-                    case "r":
-                        direction = Direction.SOUTH;
-
-                        break;
-                }
-                break;
-
-
-            case SOUTH:
-                switch ((command.toLowerCase())) {
-                    case "f":
-                        yPosition = yPosition - 1;
-
-
-                        break;
-                    case "b":
-                        yPosition = yPosition + 1;
-
-
-                        break;
-                    case "l":
-                        direction = Direction.EAST;
-
-
-                        break;
-                    case "r":
-                        direction = Direction.WEST;
-
-                        break;
-                }
-                break;
-
-            case WEST:
-                switch ((command.toLowerCase())) {
-                    case "f":
-                        xPosition = xPosition - 1;
-
-                        break;
-                    case "b":
-                        xPosition = xPosition + 1;
-
-
-                        break;
-                    case "l":
-                        direction = Direction.SOUTH;
-
-
-                        break;
-                    case "r":
-                        direction = Direction.NORTH;
-
-                        break;
-                }
-                break;
-            default:
-                throw new InvalidCommandException("Commande" + command + "inconnue");
-
+        Position posit = position;
+        Direction direction = posit.getDirection();
+        for (char instruction : (command.toLowerCase()).toCharArray()) {
+            switch (instruction) {
+                case 'f':
+                    posit = posit.movingPosition(posit.getX(), posit.getY(), direction, planetMap);
+                    break;
+                case 'b':
+                    posit = posit.movingPosition(posit.getX(), posit.getY(), direction.getOppositeDirection(), planetMap);
+                    break;
+                case 'l':
+                    direction = direction.leftSide();
+                    break;
+                case 'r':
+                    direction = direction.rightSide();
+                    break;
+                    case 's':
+                    this.shoot();
+                    break;
+                default:
+                    throw new InvalidCommandException("Commande " + command + " invalide");
+            }
         }
-        position = Position.of(xPosition,yPosition,direction);
-
-        return new Position.FixedPosition(position.getX(),position.getY(),position.getDirection());
+        return new Position.FixedPosition(posit.getX(), posit.getY(), direction);
     }
+
+    private void shoot() {
+        Position previousPosition = this.position;
+        Position tempPosition;
+        for (int i=1; i<=laserRange; i++){
+            tempPosition = this.move("f");
+            if(tempPosition.equals(previousPosition)){
+                for (Position position : this.planetMap.obstaclePositions()) {
+                    this.planetMap.deleteObstacle(position);
+                }
+                break;
+            } previousPosition = tempPosition;
+        }
+    }
+
 }
