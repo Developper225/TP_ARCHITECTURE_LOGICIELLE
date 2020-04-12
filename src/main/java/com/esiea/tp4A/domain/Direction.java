7684b65eefa0331deb976e8d7ae14bab3afdd364
Charles MAZIARSKI <charles.maziarski@outlook.fr>
2020-04-12 20:32:41 +0200

    direction file side corrected.

--- src/main/java/com/esiea/tp4A/domain/Direction.java
+++ src/main/java/com/esiea/tp4A/domain/Direction.java
@@ -1,5 +1,23 @@
 package com.esiea.tp4A.domain;
 
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST
-}
+    NORTH, EAST, SOUTH, WEST;
+
+    public Direction rightside() {
+        if (ordinal()+1 == 4)
+            return NORTH;
+        else
+            return Direction.values()[ordinal()+1];
+    }
+
+    public Direction leftside() {
+        if (ordinal()-1 == -1)
+            return WEST;
+        else
+            return Direction.values()[ordinal()-1];
+    }
+
+    public Direction getOppositeDirection(){
+        return values()[(this.ordinal()+2)%4];
+    }
+    }
