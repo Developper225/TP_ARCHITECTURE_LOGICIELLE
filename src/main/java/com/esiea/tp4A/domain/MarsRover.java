b50c36eeee932fc8a6cbfe665d98c970ca2cf312
Joe-yann CAYOL <cayol.joeyann@gmail.com>
2020-04-12 21:41:54 +0200

    fix code according to codecov.

--- src/main/java/com/esiea/tp4A/domain/MarsRover.java
+++ src/main/java/com/esiea/tp4A/domain/MarsRover.java
@@ -1,16 +1,8 @@
 package com.esiea.tp4A.domain;
 
 public interface MarsRover {
-    default MarsRover initPos(Position position) {
-        return this;
-    }
-    default MarsRover reviseMap(PlanetMap map) {
-        return this;
-    }
-    default MarsRover setLaserConf(int range) {
-        return this;
-    }
-    default Position move(String command){
-        return Position.of(0, 0, Direction.NORTH);
-    }
+     MarsRover initPos(Position position) ;
+     MarsRover reviseMap(PlanetMap map);
+     MarsRover setLaserConf(int range);
+     Position move(String command);
 }
